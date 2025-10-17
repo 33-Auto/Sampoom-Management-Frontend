baa3ddc2865ef5b5c86c066f2b66166eafeeea2f
Choosla <cnckddn0146@gmail.com>
2025-10-17 20:16:19 +0900

    chore: :lipstick: prettier 적용된 파일들 커밋

--- src/shared/lib/hooks/useUpdateEffect.test.ts
+++ src/shared/lib/hooks/useUpdateEffect.test.ts
@@ -1,17 +1,17 @@
-import { renderHook } from '@testing-library/react'
-import { vi } from 'vitest'
+import { renderHook } from "@testing-library/react";
+import { vi } from "vitest";
 
-import useUpdateEffect from './useUpdateEffect'
+import useUpdateEffect from "./useUpdateEffect";
 
-test('useUpdateEffect simulates componentDidUpdate', () => {
-  const effect = vi.fn()
-  const { rerender } = renderHook(() => useUpdateEffect(effect))
+test("useUpdateEffect simulates componentDidUpdate", () => {
+  const effect = vi.fn();
+  const { rerender } = renderHook(() => useUpdateEffect(effect));
 
-  expect(effect).toHaveBeenCalledTimes(0)
-  rerender()
-  expect(effect).toHaveBeenCalledTimes(1)
-  rerender()
-  expect(effect).toHaveBeenCalledTimes(2)
-  rerender()
-  expect(effect).toHaveBeenCalledTimes(3)
-})
+  expect(effect).toHaveBeenCalledTimes(0);
+  rerender();
+  expect(effect).toHaveBeenCalledTimes(1);
+  rerender();
+  expect(effect).toHaveBeenCalledTimes(2);
+  rerender();
+  expect(effect).toHaveBeenCalledTimes(3);
+});
