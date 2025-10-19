332dbc43e540d2cf00a9029a735a24d287e3584c
choosla <cnckddn0146@gmail.com>
2025-10-19 16:43:47 +0900

    SPM-127 : 텍스트필드 컴포넌트 UI 설계 및 기능 구현

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,3 +1,4 @@
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
 import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
@@ -15,8 +16,14 @@ const inputVariants = cva(
           'border-grey-300 dark:border-grey-700 focus:border-green-500 focus:ring-green-500',
       },
 
+      error: {
+        true: 'border-error-red focus:border-error-red focus:ring-error-red',
+        false:
+          'border-grey-300 dark:border-grey-600 focus:border-main-500 focus:ring-main-500',
+      },
+
       defaultVariants: {
-        variant: 'primary',
+        error: 'false',
       },
     },
   },
@@ -26,12 +33,78 @@ const inputVariants = cva(
 // cva로 생성한 InputVariants의 VariantProps를 상속받는다
 interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
-    VariantProps<typeof inputVariants> {}
+    VariantProps<typeof inputVariants> {
+  label?: string
+  errorText?: String
+  helperText?: String
+}
 
-// ref = React.Ref<HTMLInputElement> 를 전달받을 수 있도록 React.forwardRef를 사용한다
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ variant, ...props }, ref) => {
-    return <input ref={ref} className={inputVariants({ variant })} {...props} />
+  (
+    {
+      variant,
+      label,
+      error,
+      errorText,
+      helperText,
+      type = 'text',
+      id,
+      ...props
+    },
+    ref,
+  ) => {
+    const reactId = React.useId()
+    const inputId = id || reactId
+    const hasError = !!errorText
+    const isPassword = type === 'password'
+    const [showPassword, setShowPassword] = React.useState(false)
+    const inputType = isPassword && showPassword ? 'text' : type
+    return (
+      <div className="w-full">
+        {label && (
+          <label
+            className="block text-sm font-medium text-grey-700 dark:text-grey-300 mb-1"
+            htmlFor={inputId}
+          >
+            {label}
+          </label>
+        )}
+        <div className="relative">
+          <input
+            id={inputId}
+            ref={ref}
+            type={inputType}
+            className={inputVariants({ variant, error: hasError })}
+            {...props}
+          />
+          {isPassword && (
+            <div className="absolute inset-y-0 right-0 top-1/2  transform -translate-y-1/2 flex items-center pr-3">
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className=" bg-transparent p-0 text-grey-400 dark:text-grey-500 hover:text-grey-600 dark:hover:text-grey-300 cursor-pointer"
+              >
+                {showPassword ? (
+                  <EyeSlashIcon className="w-5 h-5" />
+                ) : (
+                  <EyeIcon className="w-5 h-5" />
+                )}
+              </button>
+            </div>
+          )}
+        </div>
+
+        {errorText && (
+          <p className="mt-1 text-sm text-error-red">{errorText}</p>
+        )}
+
+        {helperText && !errorText && (
+          <p className="mt-1 text-sm text-grey-500 dark:text-grey-400">
+            {helperText}
+          </p>
+        )}
+      </div>
+    )
   },
 )
 
