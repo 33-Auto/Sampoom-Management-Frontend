72a84e6e3c0fb0ac9b16e0b7689f60a84a553c6c
choosla <cnckddn0146@gmail.com>
2025-10-19 19:29:45 +0900

    SPM-138, SPM-139 : Table 컴포넌트 기본 구조 설계 및 스타일링 적용

--- src/pages/login/ui/Login.tsx
+++ src/pages/login/ui/Login.tsx
@@ -1,6 +1,39 @@
-import { Button } from "@/shared/ui";
+import { Button, Table } from "@/shared/ui";
 
 export default function Login() {
+  const sampleColumns = [
+    {
+      key: "name",
+      title: "Name",
+    },
+    {
+      key: "age",
+      title: "Age",
+    },
+    {
+      key: "address",
+      title: "Address",
+    },
+  ];
+
+  const sampleData = [
+    {
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+    },
+    {
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+    },
+  ];
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <div className="bg-avocado-300 mb-8 flex w-[440px] flex-col items-center gap-1 rounded-lg p-6 shadow-md">
@@ -10,6 +43,8 @@ export default function Login() {
         <Button variant="ghost">로그인</Button>
         <Button variant="outline">로그인</Button>
       </div>
+
+      <Table columns={sampleColumns} data={sampleData} />
     </div>
   );
 }
