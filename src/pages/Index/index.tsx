383d72a29f6f6cc4f1beb091eb8eb169518c6d00
choosla <cnckddn0146@gmail.com>
2025-10-19 19:29:45 +0900

    SPM-138, SPM-139 : Table 컴포넌트 기본 구조 설계 및 스타일링 적용

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -5,9 +5,43 @@ import DocList from "@/features/doc-list";
 import logo from "@/shared/assets/logo.svg";
 import Box from "@/shared/ui/Box";
 import Spinner from "@/shared/ui/Spinner";
+import { Table } from "@/shared/ui/Table";
 import styles from "@/widgets/Layout/Layout.module.css";
 
 interface Props {}
+
+const sampleColumns = [
+  {
+    key: "name",
+    title: "Name",
+  },
+  {
+    key: "age",
+    title: "Age",
+  },
+  {
+    key: "address",
+    title: "Address",
+  },
+];
+
+const sampleData = [
+  {
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+  },
+  {
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+  },
+  {
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+  },
+];
 
 const Index: React.FC<Props> = memo(() => {
   const apiKey = process.env.REACT_APP_TEXT;
@@ -24,6 +58,9 @@ const Index: React.FC<Props> = memo(() => {
         <Suspense fallback={<Spinner size="xl" />}>
           <DocList />
         </Suspense>
+      </Box>
+      <Box>
+        <Table columns={sampleColumns} data={sampleData} />
       </Box>
     </>
   );
