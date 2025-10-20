9a13caf4801871363825fc911bf36c9d99372481
choosla <cnckddn0146@gmail.com>
2025-10-21 04:01:05 +0900

    SPM-155 , SPM-156 , SPM-157 : 요청 전용 Client 생성 및 코드 리팩터링

---
+++ src/pages/login/model/useLogin.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { login } from "@/entities/user/api/auth.api";
+import { useAuthStore } from "@/entities/user/model/auth.store";
+import type { LoginRequest, LoginResponse } from "@/shared/api/models";
+
+export function useLogin() {
+  const navigate = useNavigate();
+  const { login: loginAction } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async (credentials: LoginRequest) => {
+    setIsLoading(true);
+    try {
+      const data = (await login(credentials)) as LoginResponse;
+
+      if (data.userName && data.role) {
+        loginAction(data);
+        navigate("/");
+      } else {
+        throw new Error("로그인 실패 : 잘못된 유저 정보입니다.");
+      }
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error
+          ? error.message
+          : "알 수 없는 오류가 발생했습니다.";
+      alert(`로그인 실패: ${errorMessage}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { handleLogin, isLoading };
+}
