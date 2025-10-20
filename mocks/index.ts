dc435e9d7c34a612167d2a7145aecab347d5ef9a
Choosla <cnckddn0146@gmail.com>
2025-10-20 12:07:52 +0900

    SPM-169 : 창고 주문관리 페이지 구성

---
+++ mocks/index.ts
@@ -0,0 +1,7 @@
+export * from "./factory-bom";
+export * from "./factory-employees";
+export * from "./factory-materials";
+export * from "./factory-orders";
+export * from "./warehouse-employees";
+export * from "./warehouse-inventory";
+export * from "./warehouse-orders";
