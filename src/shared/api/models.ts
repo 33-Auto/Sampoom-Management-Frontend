49b6b19fad0aadbe13effa65d193f66c4fa49dc4
Choosla <cnckddn0146@gmail.com>
2025-10-21 18:09:11 +0900

    SPM-209 : MSW 기본 핸들러 구성

--- src/shared/api/models.ts
+++ src/shared/api/models.ts
@@ -5,9 +5,13 @@ type Schemas = components["schemas"];
 
 // Auth
 export type LoginRequest = Schemas["LoginRequest"];
-export type LoginResponse = Schemas["UserResponse"];
+export type LoginResponse = Schemas["LoginResponse"];
 export type SignupRequest = Schemas["SignupRequest"];
 export type SignupResponse = Schemas["SignupResponse"];
 
 // User
 export type UserResponse = Schemas["UserResponse"];
+
+// Mock
+export type ApiResponseSignupResponse = Schemas["ApiResponseSignupResponse"];
+export type ApiResponseLoginResponse = Schemas["ApiResponseLoginResponse"];
