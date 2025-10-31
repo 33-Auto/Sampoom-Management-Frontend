aa74b8f09428691fd85b2f3a2fdc9dafcd67a2f0
Sangyoon <qlrqod123123@naver.com>
2025-10-31 10:40:31 +0900

    [FIX] 코드 래빗 리뷰 사항 수정

--- src/pages/master/items/api/items.loaders.ts
+++ src/pages/master/items/api/items.loaders.ts
@@ -3,6 +3,6 @@ import { queryClient } from "@/shared/api/query";
 import { itemsMasterQueryOptions } from "./items.api";
 
 export function loader() {
-  const itemsPromise = queryClient.ensureQueryData(itemsMasterQueryOptions);
+  const itemsPromise = queryClient.ensureQueryData(itemsMasterQueryOptions());
   return { items: itemsPromise };
 }
