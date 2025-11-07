469a5aabb49d88f76884b67b9ed1a03b7806f29b
Choosla <cnckddn0146@gmail.com>
2025-11-08 03:30:13 +0900

    fix(purchase-order) : API 명세가 바뀌어서 생긴 타입 오류 해결

--- src/entities/part/api/index.ts
+++ src/entities/part/api/index.ts
@@ -1,5 +1,3 @@
-export type { PartCategory } from "./category.api";
-export type { PartGroup } from "./group.api";
 export type { PartSearchEntry } from "./part.api";
 export { usePartCategoriesQuery } from "./category.api";
 export { usePartGroupsQuery } from "./group.api";
