06c20613623a3da534932137ee02448ea8b75bcd
Choo ChangWoo <cnckddn0146@gmail.com>
2026-03-25 20:56:41 +0900

    chore(FSD) : steiger 린터 규칙 완화

--- steiger.config.js
+++ steiger.config.js
@@ -1,14 +1,19 @@
-// ./steiger.config.js
-import fsd from '@feature-sliced/steiger-plugin'
-import { defineConfig } from 'steiger'
+import fsd from "@feature-sliced/steiger-plugin";
+import { defineConfig } from "steiger";
 
 export default defineConfig([
   ...fsd.configs.recommended,
   {
-    // disable the `public-api` rule for files in the Shared layer
-    files: ['./src/shared/**'],
     rules: {
-      'fsd/public-api': 'off',
+      "fsd/insignificant-slice": "off",
+      "fsd/excessive-slicing": "off",
+      "fsd/no-segmentless-slices": "off",
+      "fsd/inconsistent-naming": "off",
+      "fsd/segments-by-purpose": "off",
     },
   },
-])
+  {
+    files: ["./src/shared/**"],
+    rules: { "fsd/public-api": "off" },
+  },
+]);
